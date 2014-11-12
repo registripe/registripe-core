77b8a7f1b8b4ad056e7ed8430548e4fcb9c39c96
Jeremy Shipman <jeremy@burnbright.net>
2014-11-12 14:55:31 +1300

    Improved summary fields, front end fields and Title of event attendee.

--- code/model/EventAttendee.php
+++ code/model/EventAttendee.php
@@ -20,16 +20,38 @@ class EventAttendee extends DataObject{
 	public static $default_sort = "Surname ASC, FirstName ASC";
 
 	private static $summary_fields = array(
-	 	'FirstName',
-	 	'Surname',
-	 	'Email',
-	 	'Ticket.Type' => 'Title'
+	 	'FirstName' => 'First Name',
+	 	'Surname' => 'Last Name',
+	 	'Email' => 'Email',
+	 	'Ticket.Summary' => 'Ticket'
 	 );
 
 	//TODO:: test me, or perhaps only failover certian fields
 	public function __construct($record = null, $isSingleton = false, $model = null) {
 		parent::__construct($record, $isSingleton, $model);
 		$this->failover = $this->Member();
+	}
+
+	public function getFrontEndFields($params = null) {
+		$fields = parent::getFrontEndFields();
+		$fields->removeByName(array(
+			'RegistrationID', 'TicketID', 'MemberID'
+		));
+
+		return $fields;
+	}
+
+	public function getTitle(){
+		return $this->getName();
+	}
+
+	public function getName() {
+		if($this->FirstName || $this->Surname){
+			return sprintf("%s %s", $this->FirstName, $this->Surname);
+		}
+		elseif($member = $this->Member()){
+			return $member->Name;	
+		}
 	}
 
 	public function validate() {
