baec26a78e11a52a6963c3545c65049099c8f8a3
Jeremy Shipman <jeremy@burnbright.net>
2017-04-14 18:19:20 +1200

    FIX: Do not attempt payment if there is nothing to pay. Use correct function.

--- code/controllers/EventRegisterController.php
+++ code/controllers/EventRegisterController.php
@@ -209,7 +209,7 @@ class EventRegisterController extends Page_Controller {
 	*/
 	public function payment() {
 		$registration = $this->getCurrentRegistration(false);
-		if(!$registration || !$registration->getTotalOutstanding()){
+		if(!$registration || !$registration->canPay()){
 			return $this->redirect($this->Link());
 		}
 		$controller = new PaymentController($this, "payment", $registration, $registration->Total);
